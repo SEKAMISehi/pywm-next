7b5f8b2036fec55c344f9611c14567c752e13102
SEKAMISehi <sehi.chyan@gmail.com>
2025-03-30 01:43:02 +0200

    correction packages dependes; step2

--- setup.py
+++ setup.py
@@ -1,39 +1,55 @@
-import subprocess
+import os
+import sys
 import glob
 import shutil
+import subprocess
 from setuptools import setup
 
-proc = subprocess.Popen(["meson", "build"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = proc.communicate()
-if proc.returncode != 0:
-    raise Exception("Fatal: Error executing 'meson build': \n%r\n%r" % (stdout, stderr))
+# Проверяем, установлены ли meson и ninja
+def check_requirements():
+    if not shutil.which("meson"):
+        raise RuntimeError("Требуется 'meson'! Установите: pip install meson")
+    if not shutil.which("ninja"):
+        raise RuntimeError("Требуется 'ninja'! Установите: pip install ninja")
 
+def build_project():
+    # Запускаем meson build
+    proc = subprocess.run(["meson", "build"], capture_output=True, text=True)
+    if proc.returncode != 0:
+        raise RuntimeError(f"Ошибка meson:\n{proc.stderr}")
 
-proc1 = subprocess.Popen(["ninja", "-C", "build"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = proc1.communicate()
-if proc1.returncode != 0:
-    raise Exception("Fatal: Error executing 'ninja -C build': \n%r\n%r" % (stdout, stderr))
+    # Собираем через ninja
+    proc = subprocess.run(["ninja", "-C", "build"], capture_output=True, text=True)
+    if proc.returncode != 0:
+        raise RuntimeError(f"Ошибка ninja:\n{proc.stderr}")
 
-so = None
-for f in glob.glob('build/_pywm.*.so'):
-    so = f
+def copy_library():
+    # Ищем .so файл (более точный паттерн)
+    so_files = glob.glob('build/_pywm.cpython-*.so')
+    if not so_files:
+        raise FileNotFoundError("Не найдена собранная библиотека!")
 
-if so is not None:
+    dest = "pywm/_pywm.so"
+    if so_files[0] != dest:
+        shutil.copy(so_files[0], dest)
+
+if __name__ == "__main__":
     try:
-        shutil.copy(so, 'pywm/_pywm.so')
-    except shutil.SameFileError:
-        pass
-else:
-    raise Exception("Fatal: Could not find shared library")
-
-setup(name='pywm',
-      version='0.3.1.1',
-      description='wlroots-based Wayland compositor with Python frontend',
-      url="https://github.com/jbuchermn/pywm",
-      author='Jonas Bucher',
-      author_email='j.bucher.mn@gmail.com',
-      maintainer='quantum_sehi',
-      maintainer_email='117589194+SEKAMISehi@users.noreply.github.com',
-      package_data={'pywm': ['_pywm.so', 'py.typed']},
-      packages=['pywm'],
-      install_requires=['thefuzz','evdev', 'imageio', 'pycairo', 'numpy'],)
+        check_requirements()
+        build_project()
+        copy_library()
+        setup(
+            name='pywm',
+            version='0.3.1.1',
+            description='wlroots-based Wayland compositor with Python frontend',
+            url="https://github.com/jbuchermn/pywm",
+            author='Jonas Bucher',
+            author_email='j.bucher.mn@gmail.com',
+            maintainer='quantum_sehi',
+            maintainer_email='117589194+SEKAMISehi@users.noreply.github.com',
+            package_data={'pywm': ['_pywm.so', 'py.typed']},
+            packages=['pywm'],
+            install_requires=['rapidfuzz', 'evdev', 'imageio', 'pycairo', 'numpy'],
+        )
+    finally:
+        print("make successful")
